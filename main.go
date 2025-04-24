cdd38b1dd2b4bfd159715ffa94f74e00fffb28a3
Колсанов Ярослав <113019362+yaroslavkolsanov@users.noreply.github.com>
2025-04-24 13:50:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
-	"strings"
-	"time"
+
+	"github.com/gorilla/mux"
 )
 
+type Response struct {
+	Login string `json:"login"`
+}
+
 func main() {
+	router := mux.NewRouter()
 
-	http.HandleFunc("/api/rv/", reverseHandler)
+	router.HandleFunc("/login", loginHandler)
 
-	http.HandleFunc("/", dateHandler)
+	router.HandleFunc("/id/{N}", nHandler)
 
 	port := "5000"
 	if p := os.Getenv("PORT"); p != "" {
@@ -24,58 +27,33 @@ func main() {
 
 	log.Printf("Сервер запущен на порту %s", port)
 
-	err := http.ListenAndServe(":" + port, nil)
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func reverseHandler(w http.ResponseWriter, r *http.Request) {
-	prefix := "/api/rv/"
-	path := r.URL.Path
-
-	if !strings.HasPrefix(path, prefix) {
-		http.NotFound(w, r)
-		return
-	}
-	input := path[len(prefix):]
-	if input == "" {
-		http.NotFound(w, r)
-		return
-	}
-
-	matched, err := regexp.MatchString("^[a-z]+$", input)
-	if err != nil || !matched {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	reversed := reverseString(input)
-	fmt.Fprint(w, reversed)
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("yaroslavkolsanov"))
 }
 
-func reverseString(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-func dateHandler(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-
-	expectedPath := "/" + now.Format("020106")
-	if r.URL.Path != expectedPath {
-		http.NotFound(w, r)
+func nHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	n := vars["N"]
+	res, err := http.Get("https://nd.kodaktor.ru/users/" + n)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	response := map[string]string{
-		"date":  now.Format("02-01-2006"),
-		"login": "yaroslavkolsanov",
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	var resp Response
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(resp.Login))
 }
